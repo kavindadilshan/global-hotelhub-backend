31f658125042fc91a88747f07c5e770307cc8e32
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-19 02:25:31 +0530

    complete scraping for Airbnb & booking.com

---
+++ src/main/java/com/bolton/globalhotelhub/rawdata/HotelReportRawData.java
@@ -0,0 +1,12 @@
+package com.bolton.globalhotelhub.rawdata;
+
+public interface HotelReportRawData {
+//    String getLocation();
+//    String getCheckin();
+//    String getCheckout();
+//    String getAdults();
+//    String getRooms();
+//    String getChild();
+//    String getMaxPrice();
+//    String getMinPrice();
+}
--- src/main/java/com/bolton/globalhotelhub/service/impl/HotelServiceImpl.java
+++ src/main/java/com/bolton/globalhotelhub/service/impl/HotelServiceImpl.java
@@ -10,6 +10,9 @@ import com.bolton.globalhotelhub.repository.UserRepository;
 import com.bolton.globalhotelhub.service.HotelService;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -43,14 +46,18 @@ public class HotelServiceImpl implements HotelService {
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
     public List<ResponseDTO> getHotelByModel(FilterHotelRequestDTO filterHotelRequestDTO) {
         try {
-
+//            uitk-card-has-primary-theme
             List<ResponseDTO> responseDTOS = new ArrayList<>();
 
-            String BookingComUrl = String.format(ApplicationConstant.BOOKING_COM_URL, filterHotelRequestDTO.getLocation(), filterHotelRequestDTO.getCheckin(), filterHotelRequestDTO.getCheckout(), filterHotelRequestDTO.getAdults(), filterHotelRequestDTO.getRooms(), filterHotelRequestDTO.getChild(), filterHotelRequestDTO.getMaxPrice(), filterHotelRequestDTO.getMinPrice());
+            String BookingComUrl = String.format(ApplicationConstant.BOOKING_COM_URL, filterHotelRequestDTO.getLocation(), filterHotelRequestDTO.getCheckin(), filterHotelRequestDTO.getCheckout(), filterHotelRequestDTO.getAdults(), filterHotelRequestDTO.getRooms(), filterHotelRequestDTO.getChild(), filterHotelRequestDTO.getMinPrice(), filterHotelRequestDTO.getMaxPrice());
+            String HotelComUrl = String.format(ApplicationConstant.HOTEL_COM_URL, filterHotelRequestDTO.getAdults(), filterHotelRequestDTO.getChild(), filterHotelRequestDTO.getCheckin(), filterHotelRequestDTO.getCheckout(), filterHotelRequestDTO.getLocation(), filterHotelRequestDTO.getRooms(),  filterHotelRequestDTO.getMinPrice(), filterHotelRequestDTO.getMaxPrice());
+            String AirBnbUrl = String.format(ApplicationConstant.AIR_BNB_URL, filterHotelRequestDTO.getLocation(),filterHotelRequestDTO.getAdults(), filterHotelRequestDTO.getChild(), filterHotelRequestDTO.getCheckin(), filterHotelRequestDTO.getCheckout(), filterHotelRequestDTO.getRooms(),  filterHotelRequestDTO.getMinPrice(), filterHotelRequestDTO.getMaxPrice());
 
-            LOGGER.debug(BookingComUrl);
+            LOGGER.debug(AirBnbUrl);
 
             extractDataFromBookingCom(responseDTOS, BookingComUrl);
+//            extractDataFromHotelsCom(responseDTOS, HotelComUrl);
+            extractDataFromAirBnb(responseDTOS,AirBnbUrl);
 //
 //            Optional<Users> user = userRepository.findById(filterHotelRequestDTO.getUserId());
 //
@@ -70,30 +77,210 @@ public class HotelServiceImpl implements HotelService {
         try {
 
             Document document = Jsoup.connect(url).get();
-//            LOGGER.debug(document);
             Elements mainItems = document.getElementsByClass("c82435a4b8");
             for (Element object : mainItems) {
 
-                LOGGER.debug(object.getElementsByClass("a15b38c233").text());
-                LOGGER.debug(object.getElementsByTag("a").attr("href"));
-                LOGGER.debug(object.getElementsByClass("def9bc142a").text());
-                LOGGER.debug(object.getElementsByClass("e84eb96b1f").text());
-                LOGGER.debug(object.getElementsByClass("fc367255e6").text());
-                LOGGER.debug(object.getElementsByTag("img").attr("src"));
+                if (object.getElementsByClass("a15b38c233").text().length()>0){
+                    ResponseDTO responseDTO = new ResponseDTO();
+
+                    responseDTO.setTitle(object.getElementsByClass("a15b38c233").text());
+                    responseDTO.setUrl(object.getElementsByTag("a").attr("href"));
+                    responseDTO.setImage(object.getElementsByTag("img").attr("src"));
+                    responseDTO.setAddress(object.getElementsByClass("def9bc142a").text());
+                    responseDTO.setPrice(object.getElementsByClass("fc367255e6").text());
+                    responseDTO.setAmenities(object.getElementsByClass("fc367255e6").text());
+                    responseDTO.setPublishSite("booking.com");
+
+                    responseDTOS.add(responseDTO);
+                }
 
 
-                ResponseDTO responseDTO = new ResponseDTO();
+            }
+
+        } catch (IOException ex) {
+            LOGGER.error("Function : extractDataFromBookingCom  : " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    private void extractDataFromHotelsCom(List<ResponseDTO> responseDTOS, String url) {
+        try {
+
+            Document document = Jsoup.connect(url)
+                    .method(Connection.Method.GET)
+                    .get();
+            LOGGER.debug(document);
+//            Elements mainItems = document.getElementsByClass("property-listing-results");
+//            for (Element object : mainItems) {
 //
-                responseDTO.setTitle(object.getElementsByClass("a15b38c233").text());
-                responseDTO.setUrl(object.getElementsByTag("a").attr("href"));
-                responseDTO.setImage(object.getElementsByTag("img").attr("src"));
-                responseDTO.setAddress(object.getElementsByClass("def9bc142a").text());
-                responseDTO.setPrice(object.getElementsByClass("fc367255e6").text());
-                responseDTO.setAmenities(object.getElementsByClass("fc367255e6").text());
-                responseDTO.setPublishSite("booking.com");
-//
-                responseDTOS.add(responseDTO);
+//                LOGGER.debug(object.getElementsByClass("uitk-heading").text());
+//                LOGGER.debug(object.getElementsByTag("a").attr("href"));
+//                LOGGER.debug(object.getElementsByClass("def9bc142a").text());
+//                LOGGER.debug(object.getElementsByClass("e84eb96b1f").text());
+//                LOGGER.debug(object.getElementsByClass("fc367255e6").text());
+//                LOGGER.debug(object.getElementsByTag("img").attr("src"));
 
+
+//                ResponseDTO responseDTO = new ResponseDTO();
+//
+//                responseDTO.setTitle(object.getElementsByClass("a15b38c233").text());
+//                responseDTO.setUrl(object.getElementsByTag("a").attr("href"));
+//                responseDTO.setImage(object.getElementsByTag("img").attr("src"));
+//                responseDTO.setAddress(object.getElementsByClass("def9bc142a").text());
+//                responseDTO.setPrice(object.getElementsByClass("fc367255e6").text());
+//                responseDTO.setAmenities(object.getElementsByClass("fc367255e6").text());
+//                responseDTO.setPublishSite("booking.com");
+////
+//                responseDTOS.add(responseDTO);
+
+//            }
+
+        } catch (IOException ex) {
+            LOGGER.error("Function : extractDataFromBookingCom  : " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    private void extractDataFromAirBnb(List<ResponseDTO> responseDTOS, String url) {
+        try {
+
+            Document document = Jsoup.connect(url).get();
+
+            String mainItems = document.select("script#data-deferred-state").html();
+            JSONObject jsonData = new JSONObject(mainItems);
+
+
+
+            JSONArray niobeMinimalClientDataArray = jsonData.getJSONArray("niobeMinimalClientData");
+
+            // Check if the array has elements
+            if (niobeMinimalClientDataArray.length() > 0) {
+                // Access the first element of the array
+                JSONArray firstElementArray = niobeMinimalClientDataArray.getJSONArray(0);
+
+                // Check if the first element array has elements
+                if (firstElementArray.length() > 1) {
+                    // Access the second element of the first element array (which is a nested JSON object)
+                    JSONObject nestedJsonObject = firstElementArray.getJSONObject(1);
+
+                    // Check if the nested JSON object has a 'data' property
+                    if (nestedJsonObject.has("data")) {
+                        // Access the 'data' object within the nested JSON structure
+                        JSONObject dataObject = nestedJsonObject.getJSONObject("data");
+
+                        // Check if the 'data' object has a 'presentation' property
+                        if (dataObject.has("presentation")) {
+                            // Access the 'presentation' object within the 'data' object
+                            JSONObject presentationObject = dataObject.getJSONObject("presentation");
+
+                            // Check if the 'presentation' object has a 'staysSearch' property
+                            if (presentationObject.has("staysSearch")) {
+                                // Access the 'staysSearch' object within the 'presentation' object
+                                JSONObject staysSearchObject = presentationObject.getJSONObject("staysSearch");
+
+                                // Check if the 'staysSearch' object has a 'results' property
+                                if (staysSearchObject.has("results")) {
+                                    // Access the 'results' object within the 'staysSearch' object
+                                    JSONObject resultsObject = staysSearchObject.getJSONObject("results");
+
+                                    // Check if the 'results' object has a 'searchResults' property
+                                    if (resultsObject.has("searchResults")) {
+                                        // Access the 'searchResults' array within the 'results' object
+                                        JSONArray searchResultsArray = resultsObject.getJSONArray("searchResults");
+
+                                        for (Object object : searchResultsArray) {
+
+                                            JSONObject jsonDataObj = (JSONObject) object;
+
+                                            JSONObject result = jsonDataObj.getJSONObject("listing");
+                                            String bodyValue = "1 bed";
+                                            String price = "";
+
+                                            Object pictureArray = result.getJSONArray("contextualPictures");
+
+                                            JSONObject picObject = ((JSONArray) pictureArray).getJSONObject(0);
+
+                                            JSONObject pricingQuoteObj = (JSONObject) object;
+                                            JSONObject pricingQuote = pricingQuoteObj.getJSONObject("pricingQuote");
+
+                                            JSONObject priceObj = pricingQuote.getJSONObject("structuredStayDisplayPrice");
+
+
+                                            JSONObject pricePrimaryLine = priceObj.getJSONObject("primaryLine");
+
+                                            if (pricePrimaryLine.has("price")) {
+                                                price = pricePrimaryLine.getString("price");
+                                            }else {
+                                                price = pricePrimaryLine.getString("discountedPrice");
+                                            }
+
+
+                                            try {
+                                                JSONObject structuredObj = ((JSONObject) result).getJSONObject("structuredContent");
+
+                                                if (structuredObj.has("primaryLine")) {
+                                                    Object primaryLineObject = structuredObj.get("primaryLine");
+
+                                                    if (primaryLineObject instanceof JSONArray) {
+                                                        // It's an array, handle it as such
+                                                        JSONArray primaryLine = (JSONArray) primaryLineObject;
+
+                                                        if (primaryLine.length() > 0) {
+                                                            JSONObject jsonObject = primaryLine.getJSONObject(0);
+
+                                                            if (jsonObject.has("body")) {
+                                                                bodyValue = jsonObject.getString("body");
+                                                            }
+                                                        }
+                                                    } else if (primaryLineObject instanceof JSONObject) {
+                                                        // It's an object, handle it as such
+                                                        JSONObject jsonObject = (JSONObject) primaryLineObject;
+
+                                                        if (jsonObject.has("body")) {
+                                                            bodyValue = jsonObject.getString("body");
+                                                        }
+                                                    }
+                                                }
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+
+
+                                            ResponseDTO responseDTO = new ResponseDTO();
+
+                                            responseDTO.setTitle(result.getString("title"));
+                                            responseDTO.setUrl("#");
+                                            responseDTO.setImage(picObject.getString("picture"));
+                                            responseDTO.setAddress(result.getString("name"));
+                                            responseDTO.setPrice(price);
+                                            responseDTO.setAmenities(bodyValue);
+                                            responseDTO.setPublishSite("airbnb.com");
+//
+                                            responseDTOS.add(responseDTO);
+                                        }
+
+                                        // Use the 'searchResults' array as needed
+//                                        System.out.println(searchResultsArray.toString());
+                                    } else {
+                                        System.out.println("'searchResults' array not found in 'results' object");
+                                    }
+                                } else {
+                                    System.out.println("'results' object not found in 'staysSearch' object");
+                                }
+                            } else {
+                                System.out.println("'staysSearch' object not found in 'presentation' object");
+                            }
+                        } else {
+                            System.out.println("'presentation' object not found in 'data' object");
+                        }
+                    } else {
+                        System.out.println("'data' object not found in nested JSON");
+                    }
+                } else {
+                    System.out.println("Not enough elements in the first element array");
+                }
+            } else {
+                System.out.println("No elements found in 'niobeMinimalClientData' array");
             }
 
         } catch (IOException ex) {
