b22fa1b690ac9f60366544e935d5c84e1faacfea
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-19 21:13:00 +0530

    added scrape urls constance

---
+++ src/main/java/com/bolton/globalhotelhub/constant/ApplicationConstant.java
@@ -0,0 +1,21 @@
+package com.bolton.globalhotelhub.constant;
+
+public class ApplicationConstant {
+
+    /**
+     * API Response
+     */
+    public static final String API_BASE_URL = "/v1";
+
+    public static final String APPLICATION_ERROR_OCCURRED_MESSAGE = "Unexpected Error Occurred";
+
+    /**
+     * URL Pattern
+     */
+
+
+
+    public static final String HOTEL_COM_URL = "https://www.hotels.com/Hotel-Search?adults=%s&children=%s&d1=%s&d2=%s&destination=%s&rooms=%s&price=%s&price=%s";
+
+    public static final String AIR_BNB_URL = "https://www.airbnb.com/s/%s/homes?adults=%s&children=%s&checkin=%s&checkout=%s&min_bedrooms=%s&price_min=%s&price_max=%s";
+}
